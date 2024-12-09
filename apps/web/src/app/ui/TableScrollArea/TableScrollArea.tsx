cd17b738f8fde24bd96a2fc575d3fa27519fb0f4
Mary <mary.sk2000@gmail.com>
2024-12-09 23:24:35 +0300

    refactor(web): trying to find url mistake

--- apps/web/src/app/ui/TableScrollArea/TableScrollArea.tsx
+++ apps/web/src/app/ui/TableScrollArea/TableScrollArea.tsx
@@ -49,9 +49,15 @@ export function TableScrollArea() {
       const url_base = process.env.NEXT_PUBLIC_API_URL || process.env.NEXT_PUBLIC_CODESANDBOX_HOST;
       let url_codesandbox_base;
 
+      console.log('process.env.NEXT_PUBLIC_CODESANDBOX_HOST ', process.env.NEXT_PUBLIC_CODESANDBOX_HOST)
+      console.log('process.env.CODESANDBOX_HOST ', process.env.CODESANDBOX_HOST)
+      console.log('url_base ', url_base)
+
       if (url_base && !url_base.includes('localhost')) {
          url_codesandbox_base = 'https://' + url_base.replace('$PORT', '3001');
       }
+
+      console.log('url_codesandbox_base ', url_codesandbox_base)
 
       fetchJobResponses((url_codesandbox_base || url_base) + JOB_RESPONSE_URL, "GET");
    }, [])
