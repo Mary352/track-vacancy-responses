8922f46cddafd5ef3b0801c2d30d60c0f3e2d7ca
Mary <mary.sk2000@gmail.com>
2024-12-09 21:17:27 +0300

    fix(web): change url path for CodeSandbox

--- apps/web/constants/api.constants.ts
+++ apps/web/constants/api.constants.ts
@@ -1,4 +1,4 @@
-export const JOB_RESPONSE_URL = '/vacancyresponse'
+export const JOB_RESPONSE_URL = 'vacancyresponse'
 export const JOB_RESPONSE_ACTIONS_URLS = {
    update: '/update',
    delete: '/delete'
